6e57be8a70dac9de22184e5e623e57ea3a93a107
Juan Miranda <juanddmiranda@gmail.com>
2019-03-13 17:42:38 -0700

    API Rest Full ADQ
    Adquiring information from API Service.

---
+++ Sales.Domain/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Sales.Domain
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Sales.Common.Models.Products> Products { get; set; }
+    }
+}
